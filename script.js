679bddf5dee29b784c947396efeacddab338b35e
imsp18 <shivangparmar537@gmail.com>
2024-04-22 13:09:32 +0530

    Refactor script.js for displaying Pokemon data in a list

--- script.js
+++ script.js
@@ -13,21 +13,50 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
   .then((data) => {
     allPokemons = data.results;
     console.log(allPokemons);
+    displayPokemons(allPokemons);
   });
+//fetching the data from the API
 
 async function fetchPokemonDataBeforeRedirect(id) {
   try {
     const [pokemon, pokemonSpecies] = await Promise.all([
       fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
         res.json()
-      ),
+      ), //fetching the pokemon data
       fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
         res.json()
-      ),
+      ), //fetching the pokemon species data
     ]);
     return true;
   } catch (error) {
-    console.log("Failed to Fetch the data before the redirect");
+    console.log("Failed to Fetch the data before the redirect"); //if the data fetching fails then this message will be displayed
   }
 }
 
+function displayPokemons(pokemon) {
+  listWrapper.innerHTML = ""; //clearing the list wrapper before displaying the pokemons again
+  pokemon.forEach((pokemon) => {
+    const pokemonID = pokemon.url.split("/")[6]; //getting the pokemon id from the url
+    const listItem = document.createElement("div");
+    listItem.className = "list-item";
+    listItem.innerHTML = `
+        <div class="number-wrap">
+            <p class="caption-fonts">#${pokemonID}</p>
+        </div>
+        <div class="image-wrap">
+            <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}"/>
+        </div>
+        <div class="name-wrap">
+            <p class="body3-fonts">${pokemon.name}</p>
+        </div>
+        `; //displaying the pokemon data in the list wrapper
+
+    listItem.addEventListener("click", async () => {
+      const success = await fetchPokemonDataBeforeRedirect(pokemonID);
+      if (success) {
+        window.location.href = `./detail.html?=${pokemonID}`;
+      }
+    });
+    listWrapper.appendChild(listItem);
+  });
+}
